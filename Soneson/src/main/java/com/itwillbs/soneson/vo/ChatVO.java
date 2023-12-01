d47762220bc2deba54ddfe9ce788220523d662c1
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-12-01 11:33:37 +0900

    [김주영 VO 추가]

---
+++ Soneson/src/main/java/com/itwillbs/soneson/vo/ChatVO.java
@@ -0,0 +1,22 @@
+package com.itwillbs.soneson.vo;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class ChatVO {
+	
+	private String room_idx;		// 채팅방 번호
+    private String user_id;			// 구매자(유저) 아이디(닉네임/이름)
+    
+    private String creator_id;		// 판매자(창작자) 아이디(닉네임/이름)
+    private String creator_profile;	// 상대방 프로필 사진(대화 내용에 나오는게 아닌 대화창 화면에 표기할 용도)
+    
+//    private int unread_count;		// 안 읽은 메세지 수
+    
+    private Timestamp date;			// 메세지 주고 받는 일자(년월일)
+    								// 발송 시각은?(오후 몇시 몇분)
+    								// 읽음 표시는?(1 또는 사라짐)
+}
+
