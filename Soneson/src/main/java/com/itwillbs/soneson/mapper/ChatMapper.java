6a46fd4ab061e7bcd671fd430bcc6a807ceed2b0
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-12-15 19:51:13 +0900

    [김주영 chatting 기능 완료 css 수정중]

--- Soneson/src/main/java/com/itwillbs/soneson/mapper/ChatMapper.java
+++ Soneson/src/main/java/com/itwillbs/soneson/mapper/ChatMapper.java
@@ -10,20 +10,19 @@ import com.itwillbs.soneson.vo.ChatRoomListVO;
 
 @Mapper
 public interface ChatMapper {
-	
-	// 채팅방 저장 요청
+
 	void insertChatRoom(List<ChatRoomListVO> chatRoomList);
-	
-	// 채팅방 목록 조회 요청
+
 	List<Map<String, String>> selectChatRoomList(String sender_id);
-	
-	// 채팅방 채팅 내역 조회 요청
+
 	List<ChatMessage2> selectChatList(String room_id);
-	
-	// 채팅 메세지 저장 요청
+
 	void insertMessage(ChatMessage2 chatMessage);
-	
-	// 채팅방 사용자 삭제 요청
+
 	int deleteChatRoomUser(ChatMessage2 chatMessage);
+
+	int selectCurrentChatRoomUserCountFromRoomId(ChatMessage2 chatMessage);
+
+	void updateChatRoomUser(ChatMessage2 chatMessage);
 
 }
